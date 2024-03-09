6db779c37dc5b3817a9d5178355a959c2d332a62
[AloneAsif] <[asifleon3@gmail.com]>
2024-03-09 14:32:41 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+"use strict";
+// Question no # 9      Favorite Number
+let fav_number = 7;
+console.log(`my favorate number is ${fav_number}`);
---
+++ main.ts
@@ -0,0 +1,4 @@
+// Question no # 9      Favorite Number
+
+let fav_number: number = 7;
+console.log(`my favorate number is ${fav_number}`);
